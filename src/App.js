7e7fa1bf41daf06d3cf83de377eb2263ee1a27a4
arinsoni <arinsoni.iitb@gmail.com>
2023-05-18 06:49:39 +0530

    Infinite scroll Added

--- src/App.js
+++ src/App.js
@@ -14,13 +14,15 @@ export default class App extends Component {
         <Router>
           <Navbar />
           <Routes>
-            <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="General"/>} />
+            <Route exact path="/" element={<News key="general" pageSize={this.pageSize} country="in" category="General"/>} />
             <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="Business" />} />
+            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="Entertainment" />} />
+            <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="General" />} />
+            <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="Health" />} />
             <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="Science" />} />
             <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="Sports" />} />
-            <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="Health" />} />
             <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="Technology" />} />
-            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="Entertainment" />} />
+            
           </Routes>
         </Router>
       </div>
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,12 +4,14 @@ import loading from './loading.gif'
 export class Spinner extends Component {
   render() {
     return (
-      <div className="text-center" style={{ position: 'absolute', zIndex: '9', top: '50%', left: '50%'}}>
+      <div className="text-center">
         <img src={loading} alt="loading" />
       </div>
     )
   }
 }
+
+// style={{ position: 'absolute', zIndex: '9', top: '50%', left: '50%'}}
 
 export default Spinner
 
