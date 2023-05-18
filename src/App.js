a0b9b03f036d48a73852bcc79a5945403ebe7f3c
arinsoni <arinsoni.iitb@gmail.com>
2023-05-18 14:24:23 +0530

    Class Based complete

--- src/App.js
+++ src/App.js
@@ -4,25 +4,41 @@ import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
 
 
 export default class App extends Component {
+
+  apiKey = process.env.REACT_APP_NEWS_API;
   pageSize = 10;
+  state = {
+    progress: 0
+  }
+
+  setProgress= (progress)=>{
+    this.setState({progress: progress})
+  }
   render() {
     return (
       <div>
         <Router>
           <Navbar />
+          <LoadingBar
+          height={3}
+            color='#f11946'
+            progress={this.state.progress}
+          />
           <Routes>
-            <Route exact path="/" element={<News key="general" pageSize={this.pageSize} country="in" category="General"/>} />
-            <Route exact path="/business" element={<News key="business" pageSize={this.pageSize} country="in" category="Business" />} />
-            <Route exact path="/entertainment" element={<News key="entertainment" pageSize={this.pageSize} country="in" category="Entertainment" />} />
-            <Route exact path="/general" element={<News key="general" pageSize={this.pageSize} country="in" category="General" />} />
-            <Route exact path="/health" element={<News key="health" pageSize={this.pageSize} country="in" category="Health" />} />
-            <Route exact path="/science" element={<News key="science" pageSize={this.pageSize} country="in" category="Science" />} />
-            <Route exact path="/sports" element={<News key="sports" pageSize={this.pageSize} country="in" category="Sports" />} />
-            <Route exact path="/technology" element={<News key="technology" pageSize={this.pageSize} country="in" category="Technology" />} />
-            
+            <Route exact path="/" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="General" />} />
+            <Route exact path="/business" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="business" pageSize={this.pageSize} country="in" category="Business" />} />
+            <Route exact path="/entertainment" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" pageSize={this.pageSize} country="in" category="Entertainment" />} />
+            <Route exact path="/general" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="general" pageSize={this.pageSize} country="in" category="General" />} />
+            <Route exact path="/health" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="health" pageSize={this.pageSize} country="in" category="Health" />} />
+            <Route exact path="/science" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="science" pageSize={this.pageSize} country="in" category="Science" />} />
+            <Route exact path="/sports" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" pageSize={this.pageSize} country="in" category="Sports" />} />
+            <Route exact path="/technology" element={<News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" pageSize={this.pageSize} country="in" category="Technology" />} />
+
           </Routes>
         </Router>
       </div>
--- src/components/News.js
+++ src/components/News.js
@@ -31,15 +31,19 @@ export class News extends Component {
     }
 
     async updateNews() {
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c5e09acb8c4741f8b5fc60d81de15f13&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.props.setProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json()
+        this.props.setProgress(70);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false, 
         })
+        this.props.setProgress(100);
 
     }
     async componentDidMount() {
@@ -58,7 +62,7 @@ export class News extends Component {
 
     fetchMoreData = async () => {  
         this.setState({page: this.state.page + 1})
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=c5e09acb8c4741f8b5fc60d81de15f13&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         this.setState({
@@ -83,7 +87,7 @@ export class News extends Component {
                     <div className="row">
                         {this.state.articles.map((element) => {
                             return <div className="col-md-4" key={element.url}>
-                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name ? element.source.name : "" } />
                             </div>
                         })}
                     </div>
